37f5374069351ffd2015ee5e3f9368245d670087
Paul Boyd <pboyd@cpan.org>
2011-05-23 12:33:18 +0000

    Import of PBOYD/MooseX-Test-Role-0.02 from CPAN.
    gitpan-cpan-distribution: MooseX-Test-Role
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         PBOYD/MooseX-Test-Role-0.02.tar.gz
    gitpan-cpan-author:       PBOYD
    gitpan-cpan-maturity:     released

--- lib/MooseX/Test/Role.pm
+++ lib/MooseX/Test/Role.pm
@@ -1,6 +1,6 @@
 package MooseX::Test::Role;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use strict;
 use warnings;
@@ -8,26 +8,45 @@ use warnings;
 use List::Util qw/first/;
 use Test::Builder;
 use Moose qw//;
+use Carp;
 
 use Exporter qw/import unimport/;
-our @EXPORT = qw/requires_ok/;
+our @EXPORT = qw/requires_ok consumer_of/;
 
 sub requires_ok {
-    my ( $class, @required ) = @_;
-    my $msg = "$class requires " . join( ', ', @required );
+    my ( $role, @required ) = @_;
+    my $msg = "$role requires " . join( ', ', @required );
 
-    if ( !$class->can('meta') || !$class->meta->isa('Moose::Meta::Role') ) {
+    if ( !$role->can('meta') || !$role->meta->isa('Moose::Meta::Role') ) {
         ok( 0, $msg );
         return;
     }
 
     foreach my $req (@required) {
-        unless (first { $_ eq $req } $class->meta->get_required_method_list) {
-            ok(0, $msg);
+        unless ( first { $_ eq $req } $role->meta->get_required_method_list ) {
+            ok( 0, $msg );
             return;
         }
     }
-    ok(1, $msg);
+    ok( 1, $msg );
+}
+
+sub consumer_of {
+    my ( $role, %methods ) = @_;
+
+    if ( !$role->can('meta') || !$role->meta->isa('Moose::Meta::Role') ) {
+        confess 'first argument to consumer_of should be a role';
+    }
+
+    $methods{$_} ||= sub { undef }
+      for $role->meta->get_required_method_list;
+
+    my $meta = Moose::Meta::Class->create_anon_class(
+        roles   => [$role],
+        methods => \%methods,
+    );
+
+    return $meta->new_object;
 }
 
 my $Test = Test::Builder->new;
@@ -51,14 +70,30 @@ MooseX::Test::Role - Test functions for Moose roles
 
   requires_ok('MyRole', qw/method1 method2/);
 
+  my $consumer = consumer_of('MyRole', method1 => sub { 1 });
+  ok($consumer->myrole_method);
+  is($consumer->method1, 1);
+
 =head1 DESCRIPTION
 
-Provides functions for testing roles. Right now the only method is
-C<requires_ok>.
+Provides functions for testing roles.
 
-=head1 FUNCTIONS
+=head1 EXPORTED FUNCTIONS
 
 =over 4
+
+=item B<consumer_of ($role, %methods)>
+
+Creates an instance of a class which consumes the role. Required methods are
+stubbed, they return undef by default.
+
+To add additional methods to the instance specify the name and coderef:
+
+  consumer_of('MyRole',
+      method1 => sub { 'one' },
+      method2 => sub { 'two' },
+      required_method => sub { 'required' },
+  );
 
 =item B<requires_ok ($role, @methods)>
 
